041b8f620ef35b9d5ea1e8919b5d1b9ff44d5de4
Janadhi Dissanayake <janadhi14@gmail.com>
2022-07-10 20:24:00 +1200

    first commit

---
+++ testing1.java
@@ -0,0 +1,10 @@
+public class testing1 {
+    public static void main(String[] args) {
+        create_for_loop(5);
+    }
+    public static void create_for_loop(int x){
+        for(int i = 0; i < x; i++){
+            System.out.println("Hello");
+        }
+    }
+}
