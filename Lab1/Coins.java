6186bfa2e1373b36aaffc4db7bd503ec07985bb0
Janadhi Dissanayake <janadhi14@gmail.com>
2022-07-11 13:34:52 +1200

    added Coins files

---
+++ Lab1/Coins.java
@@ -0,0 +1,20 @@
+/**
+* A class to represent a series of coin tosses and 
+* provide methods to supply information about them.
+* @author Nick Meek
+*/
+public class Coins{
+
+    public static final boolean HEADS = true;
+    public static final boolean TAILS = false;
+
+    private boolean[] coins;
+
+    public Coins(boolean[] coins) {
+        this.coins = coins;
+    }
+    public static void countHeads(){
+        for(int 1 = 0l)
+    }
+
+}
---
+++ Lab1/CoinsApp.java
@@ -0,0 +1,13 @@
+/**
+* An application class to test the Coins class
+* @author Nick Meek
+*/
+public class CoinsApp{
+
+   public static void main(String[] args){
+      boolean[] coinTosses = {true, false, true, false, true, true, false, false, false, true};
+      
+     Coins c = new Coins(coinTosses);
+     System.out.println(c.countHeads());
+  }
+}
