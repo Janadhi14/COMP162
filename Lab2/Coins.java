100620471d5a746fcb0f0dd54cab1fe8a1b8ffef
Janadhi Dissanayake <janadhi14@gmail.com>
2022-07-12 12:20:02 +1200

    forloop written

--- Lab2/Coins.java
+++ Lab2/Coins.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
 * A class to represent a series of coin tosses and 
 * provide methods to supply information about them.
@@ -37,6 +38,31 @@ public class Coins{
         }return(s); // returns a string 
 
     }
+
+
+    /*creates a Coins object from a String consisting entirely of the characters H and T  */
+    public boolean [] Coins(String c){
+        // we need to use a scanner to go through the line and ever character in the line,
+        // create a new array that will turn the char 
+        Scanner sc = new Scanner(c); // scanning thorugh the string
+        // need to create an array that will be the lenght of the string 
+        char[] n = new char[c.length()];
+        for (int j = 0; j <= l - 1; j++) 
+        { char c1 = c.charAt(j);
+           n[j] = c1;
+        }
+        // now we want to go through teh char array and see if it 
+        return n;
+        // now we need to create a loop which will go through that array and create a new array that/ that is of the same length and corresponds to the 
+
+    }
+
     
+    /*Constructs a Coins object consisting of a series of length coins */
+    public Coins(int length){
+
+    }
+
+    /*countRuns() Returns an int which is the number of runs in this sequence of coins (a run is a block of coins all showing the same face, so for example in HHTHHHTTT there are four runs namely HH, T, HHH, and TTT. */
 
 }
