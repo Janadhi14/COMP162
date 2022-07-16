788d6fe2445b91e80d4c04a8164c4db2e7adca77
Janadhi Dissanayake <janadhi14@gmail.com>
2022-07-17 11:22:54 +1200

    completed bicycle and started AntJourney

---
+++ Lab3/AntJourney.java
@@ -0,0 +1,12 @@
+public class AntJourney{
+    private String antId;
+    private Point[] journey;
+
+    AntJourney(){}
+
+    AntJourney(String id, int numPoints){
+        this.id = id;
+        this.m
+    }
+    
+}
