05dfe0808ebdde75cebdca16677f281acd5d594f
Janadhi Dissanayake <janadhi14@gmail.com>
2022-07-18 19:08:53 +1200

    Created a new maker crypto plotting file

--- Lab3/AntJourney.java
+++ Lab3/AntJourney.java
@@ -29,7 +29,7 @@ public class AntJourney{
         return shortest;
     }
     public double getLongestDistance(){
-        for(ant )
+        for(ant : )
         
         
         return longest;
