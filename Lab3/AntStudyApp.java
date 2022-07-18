86dd4d9201f269a32d124702eaaaff75a1e04d30
Janadhi Dissanayake <janadhi14@gmail.com>
2022-07-18 17:40:10 +1200

    progress

--- Lab3/AntStudyApp.java
+++ Lab3/AntStudyApp.java
@@ -1,5 +1,7 @@
+import java.text.DecimalFormat;
 public class AntStudyApp{
     public static void main(String[] Args){
         AntJourney test_ant = new AntJourney("test ant",);
     }
 }
+//the format of the input for an ant:  Blue Ant: 2,1: 3,4: 1,6: 4,7: 6,4: 3,1: 1,4 
