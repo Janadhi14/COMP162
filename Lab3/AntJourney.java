06aef4f15c8388b24ad0b828a781675c73d201f6
Janadhi Dissanayake <janadhi14@gmail.com>
2022-07-22 10:32:46 +1200

    removed extra method for creating an array

--- Lab3/AntJourney.java
+++ Lab3/AntJourney.java
@@ -90,20 +90,5 @@ public class AntJourney {
         return (andId + );
     }
 
-    public static void splitter(String coordinates){
-        String[] split = coordinates.split(":",-1);
-        // now we have a string array that is split by the ":"
-        // lets use a for loop now to go through the split array and split into a new array 
-        // create a new array that is 2 times the length of the split array  
-        String[] intCor = new String[2*split.length];
-        
-        for(int i = 0; i < intCor.length; i = i+2){
-            intCor[i] = split[i].split(":", -1);
-            // this integer is 
-            int  
 
-            // we 
-        }
-        
-        
 }
